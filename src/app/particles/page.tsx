71d4f8b72b945c581486ecc71cee17f8b676a8b6
aimakov <nurbolat.aimakov@gmail.com>
2023-06-09 03:24:46 +0900

    Completed example

--- src/app/particles/page.tsx
+++ src/app/particles/page.tsx
@@ -12,21 +12,25 @@ class Particle {
     speedY: any;
     history: any[];
     maxLength: number;
-    angle = 0;
+    angle: number;
+    speedModifier: number;
+    timer: number;
 
     constructor(effect: any) {
         this.effect = effect;
         this.x = Math.floor(Math.random() * this.effect.width);
         this.y = Math.floor(Math.random() * this.effect.height);
-        this.speedX = Math.random() * 5 - 2.5;
-        this.speedY = Math.random() * 5 - 2;
+        this.speedX;
+        this.speedY;
+        this.speedModifier = Math.floor(Math.random() * 5 + 1);
         this.history = [{ x: this.x, y: this.y }];
-        this.maxLength = Math.floor(Math.random() * 100 + 10);
+        this.maxLength = Math.floor(Math.random() * 200 + 10);
         this.angle = 0;
+        this.timer = this.maxLength * 2;
     }
 
     draw(context: any) {
-        context.fillRect(this.x, this.y, 10, 10);
+        // context.fillRect(this.x, this.y, 10, 10);
         context.beginPath();
         context.moveTo(this.history[0].x, this.history[0].y);
 
@@ -38,13 +42,39 @@ class Particle {
     }
 
     update() {
-        this.angle += 0.5;
-        this.x += this.speedX * Math.sin(this.angle) * 3;
-        this.y += this.speedY * Math.cos(this.angle);
-        this.history.push({ x: this.x, y: this.y });
-        if (this.history.length > this.maxLength) {
+        this.timer--;
+
+        if (this.timer >= 1) {
+            let x = Math.floor(this.x / this.effect.cellSize);
+            let y = Math.floor(this.y / this.effect.cellSize);
+
+            let index = y * this.effect.cols + x;
+            this.angle = this.effect.flowField[index];
+
+            this.speedX = Math.cos(this.angle);
+            this.speedY = Math.sin(this.angle);
+
+            this.x += this.speedX * this.speedModifier;
+            this.y += this.speedY * this.speedModifier;
+
+            this.history.push({ x: this.x, y: this.y });
+            if (this.history.length > this.maxLength) {
+                this.history.shift();
+            }
+        } else if (this.history.length > 1) {
             this.history.shift();
+        } else {
+            this.reset();
         }
+    }
+
+    reset() {
+        this.x = Math.floor(Math.random() * this.effect.width);
+        this.y = Math.floor(Math.random() * this.effect.height);
+
+        this.history = [{ x: this.x, y: this.y }];
+
+        this.timer = this.maxLength * 2;
     }
 }
 
@@ -53,16 +83,39 @@ class Effect {
     height: number;
     particles: any[];
     numberOfParticles: number;
+    cellSize: number;
+    rows: number;
+    cols: number;
+    flowField: number[];
+    curve: number;
+    zoom: number;
 
     constructor(width: number, height: number) {
         this.width = width;
         this.height = height;
         this.particles = [];
-        this.numberOfParticles = 50;
+        this.numberOfParticles = 2000;
+        this.cellSize = 20;
+        this.rows;
+        this.cols;
+        this.flowField = [];
+        this.curve = 5;
+        this.zoom = 0.11;
         this.init();
     }
 
     init() {
+        this.rows = Math.floor(this.height / this.cellSize);
+        this.cols = Math.floor(this.width / this.cellSize);
+        this.flowField = [];
+
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve;
+                this.flowField.push(angle);
+            }
+        }
+
         for (let i = 0; i < this.numberOfParticles; i++) {
             this.particles.push(new Particle(this));
         }
